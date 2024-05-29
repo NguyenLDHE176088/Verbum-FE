c0f25dc06f925e10dc77082b499b595f02d3002c
laamwwibu <lampphe172382@fpt.edu.vn>
2024-05-29 23:10:11 +0700

    View All Jobs Screen with mock data

---
+++ src/components/screens/jobs/components/utilsbar.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import { ChevronDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+
+interface UtilsBarProps {
+  table: any; // replace 'any' with the actual type of your 'table' object
+}
+
+const UtilsBar: React.FC<UtilsBarProps> = ({ table }) => {
+  return (
+    <div className="flex items-center py-4">
+      <Input
+        placeholder="Filter filename..."
+        value={
+          (table.getColumn("filename")?.getFilterValue() as string) ?? ""
+        }
+        onChange={(event) =>
+          table.getColumn("filename")?.setFilterValue(event.target.value)
+        }
+        className="max-w-sm"
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="ml-auto">
+            Columns <ChevronDown className="ml-2 h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          {table
+            .getAllColumns()
+            .filter((column) => column.getCanHide())
+            .map((column) => {
+              return (
+                <DropdownMenuCheckboxItem
+                  key={column.id}
+                  className="capitalize"
+                  checked={column.getIsVisible()}
+                  onCheckedChange={(value) =>
+                    column.toggleVisibility(!!value)
+                  }
+                >
+                  {column.id}
+                </DropdownMenuCheckboxItem>
+              );
+            })}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default UtilsBar;
