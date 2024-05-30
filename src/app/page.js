97c755c842eb752f3f19e57f6db2713644e1cfdb
TruongPDHE170417 <truongpdhe170417@fpt.edu.vn>
2024-05-30 07:43:04 +0700

    apply stash change when resolve confict

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
+
+
+import '@/components/common/NavBar'
+import NavBar from "@/components/common/NavBar";
+
 
 export default function Home() {
   return (
-   <></>
+    <NavBar />  
   );
 }
