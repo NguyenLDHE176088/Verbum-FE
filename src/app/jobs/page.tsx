0138765ed0b793493c47befebd995d98a1ec7718
laamwwibu <lampphe172382@fpt.edu.vn>
2024-06-04 20:35:27 +0700

    add layout and navbar

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { JobsScreen } from "@/components/screens/jobs";
+import { MainLayout } from "@/components/layouts/MainLayout";
 
 const JobPage = () => {
-  return <JobsScreen />;
+  return (
+    <MainLayout>
+      <JobsScreen />
+    </MainLayout>
+  );
 };
 
 export default JobPage;
