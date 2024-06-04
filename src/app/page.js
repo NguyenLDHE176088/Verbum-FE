b09dae7110c62bcca6f2a694b424935198246743
TruongPDHE170417 <truongpdhe170417@fpt.edu.vn>
2024-06-04 20:39:50 +0700

    update project detail development

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable"
 
 
-import '@/components/common/NavBar'
-import NavBar from "@/components/common/NavBar";
-
-import {Toaster} from "@/components/ui/toaster";
 
 export default function Home() {
   return (
-    <NavBar />  
-  );
+  <></>
+  )
 }
---
+++ src/app/projects/detail/page.jsx
@@ -0,0 +1,12 @@
+import ProjectInfo from '@/components/project/detail/ProjectInfo'
+import JobTable from '@/components/project/detail/JobTable'
+
+
+export default function ProjectDetail() {
+    return (
+        <>
+            <ProjectInfo />
+            <JobTable />
+        </>
+    )
+}
