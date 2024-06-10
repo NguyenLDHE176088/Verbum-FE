8f9f62d543af47eb975240bce8df9a47167dd807
laamwwibu <lampphe172382@fpt.edu.vn>
2024-06-10 22:49:30 +0700

    add breadcrumbs

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -25,7 +25,7 @@ export function Navbar() {
         </Link>
         <Link
           href="/users"
-          className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+          className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
         >
           <Package className="h-4 w-4" />
           Users
