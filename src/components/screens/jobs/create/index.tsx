e5b7762f76cbbca0821417d8c604b5dfd6a0088d
laamwwibu <lampphe172382@fpt.edu.vn>
2024-07-01 12:11:28 +0700

    added QA steps checkbox when create jobs

--- src/components/screens/jobs/create/index.tsx
+++ src/components/screens/jobs/create/index.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { format } from "date-fns";
+import { format, set } from "date-fns";
 import { CalendarIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -30,6 +30,13 @@ import app from "@/lib/firebaseConfig";
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import { getUserIdFromCookie } from "@/lib/auth";
 import { Language } from "@/models/languages";
+import {
+  fetchProjectSourceLanguage,
+  fetchProjectTargetLanguages,
+} from "@/data/projects";
+import { fetchUserCompany } from "@/data/company";
+import { fetchUsersBySourceAndTargetLanguage } from "@/data/users";
+import { createJob } from "@/data/job";
 
 const FormSchema = z.object({
   file: z.custom(
@@ -58,10 +65,10 @@ const FormSchema = z.object({
   ),
   target_languages: z.array(z.any()),
   duedate: z.date(),
+  isQaNeeded: z.array(z.boolean().optional()),
 });
 
-
-export function CreateJobScreen({projectId}) {
+export function CreateJobScreen({ projectId }) {
   const [userId, setUserId] = useState<string>();
   const [userCompany, setUserCompany] = useState<string>();
   const [projectSourceLanguage, setProjectSourceLanguage] =
@@ -69,6 +76,7 @@ export function CreateJobScreen({projectId}) {
   const [projectTargetLanguages, setProjectTargetLanguages] = useState([]);
   const [selectedTargetLanguages, setSelectedTargetLanguages] = useState([]);
   const [checkedLanguages, setCheckedLanguages] = useState({});
+  const [isQaNeededState, setIsQaNeededState] = useState({});
   const [selectedUsers, setSelectedUsers] = useState({});
   const router = useRouter();
 
@@ -78,52 +86,22 @@ export function CreateJobScreen({projectId}) {
     return response;
   };
 
-  const fetchUserCompany = async (userId: string) => {
-    try {
-      const response = await fetch(
-        `http://localhost:9999/company/find-by-user-id?userId=${userId}`
-      );
-      const data = await response.json();
-      setUserCompany(data.companyId);
-    } catch (error) {
-      console.error("Error fetching user company:", error);
-    }
-  };
-
-  const fetchProjectSourceLanguage = async (projectId: string) => {
-    try {
-      const response = await fetch(
-        `http://localhost:9999/languages/source-language?projectId=${projectId}`
-      );
-      const data = await response.json();
-      setProjectSourceLanguage(data.language);
-    } catch (error) {
-      console.error("Error fetching project source language:", error);
-    }
-  };
-
-  const fetchProjectTargetLanguages = async (projectId: string) => {
-    try {
-      const response = await fetch(
-        `http://localhost:9999/languages/target-language?projectId=${projectId}`
-      );
-      const data = await response.json();
-      setProjectTargetLanguages(data);
-    } catch (error) {
-      console.error("Error fetching project target languages:", error);
-    }
-  };
-
   useEffect(() => {
     const initialize = async () => {
       const userId = await getUserId();
       if (userId) {
-        await fetchUserCompany(userId);
+        await Promise.all([
+          fetchUserCompany(userId).then((data) => setUserCompany(data)),
+        ]);
       }
       if (projectId) {
         await Promise.all([
-          fetchProjectSourceLanguage(projectId as string),
-          fetchProjectTargetLanguages(projectId as string),
+          fetchProjectSourceLanguage(projectId as string).then((data) =>
+            setProjectSourceLanguage(data.language)
+          ),
+          fetchProjectTargetLanguages(projectId as string).then((data) =>
+            setProjectTargetLanguages(data)
+          ),
         ]);
       }
     };
@@ -147,34 +125,33 @@ export function CreateJobScreen({projectId}) {
     }));
 
     if (!checkedLanguages[id]) {
-      try {
-        const response = await fetch(
-          `http://localhost:9999/jobs/find-by-source-target-language?companyId=${userCompany}&sourceLanguageCode=${projectSourceLanguage.code}&targetLanguageCode=${id}`
-        );
-        if (!response.ok) {
-          throw new Error("Failed to fetch target languages");
-        }
-
-        const data = await response.json();
-
-        // Add the fetched language to selectedTargetLanguages
-        setSelectedTargetLanguages((prev) => [...prev, data]);
-      } catch (error) {
-        console.error("Error fetching target languages:", error);
-      }
+      await Promise.all([
+        fetchUsersBySourceAndTargetLanguage(
+          userCompany,
+          projectSourceLanguage.code,
+          id
+        ).then((data) => {
+          setSelectedTargetLanguages((prev) => [...prev, data]);
+        }),
+      ]);
     } else {
-      // Remove the language from selectedTargetLanguages if the checkbox is unchecked
       setSelectedTargetLanguages((prev) =>
         prev.filter((lang) => lang.code !== id)
       );
     }
   };
 
-
   const handleUserSelection = (code, users) => {
     setSelectedUsers((prev) => ({
       ...prev,
       [code]: users,
+    }));
+  };
+
+  const handleQaCheckboxChange = (id) => {
+    setIsQaNeededState((prev) => ({
+      ...prev,
+      [id]: !prev[id],
     }));
   };
 
@@ -191,13 +168,10 @@ export function CreateJobScreen({projectId}) {
         "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
     };
 
-    // Upload the file to the path 'some-child'
     const snapshot = await uploadBytes(storageRef, file, metadata);
-
 
     // Get the download URL of the uploaded file
     const downloadURL = await getDownloadURL(storageRef);
-
 
     Object.keys(checkedLanguages).forEach((languageId) => {
       if (checkedLanguages[languageId]) {
@@ -210,24 +184,15 @@ export function CreateJobScreen({projectId}) {
           fileExtension: `.${data.file.name.split(".").pop()}`,
           status: "new",
           documentUrl: downloadURL,
+          isQaNeeded: isQaNeededState[languageId] || false,
         };
 
         jobsPayload.push(jobPayload);
       }
     });
-    const response = await fetch("http://localhost:9999/jobs/create", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(jobsPayload),
-    });
+    createJob(jobsPayload);
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    window.alert("Request was successful!");
+    window.alert("Job created successfully!");
     router.push("/jobs");
   };
 
@@ -258,54 +223,82 @@ export function CreateJobScreen({projectId}) {
           )}
         />
         {projectTargetLanguages.map((language, index) => (
-          <FormField
-            key={index}
-            control={form.control}
-            name={`target_languages[${index}].name` as any}
-            render={() => (
-              <FormItem>
-                <Checkbox
-                  className="mr-3"
-                  checked={!!checkedLanguages[language.code]}
-                  onCheckedChange={() => handleCheckboxChange(language.code)}
-                />
-                <FormLabel>{language.name}</FormLabel>
-                <FormControl>
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <Button
-                        variant="outline"
-                        className="justify-start ml-3"
-                        disabled={!checkedLanguages[language.code]}
-                      >
-                        {selectedUsers[language.code]?.length > 0
-                          ? `${selectedUsers[language.code]
-                              .map(
-                                (user) => user.lastName + " " + user.firstName
-                              )
-                              .join(", ")}`
-                          : "Choose linguists"}
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-[200px] p-0" align="start">
-                      <UserList
-                        users={
-                          selectedTargetLanguages.find(
-                            (lang) => lang.code === language.code
-                          )?.users || []
-                        }
-                        selectedUsers={selectedUsers[language.code] || []}
-                        onUserSelection={(users) =>
-                          handleUserSelection(language.code, users)
-                        }
-                      />
-                    </PopoverContent>
-                  </Popover>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+          <>
+            <FormField
+              key={index}
+              control={form.control}
+              name={`target_languages[${index}].name` as any}
+              render={() => (
+                <FormItem>
+                  <Checkbox
+                    className="mr-3"
+                    checked={!!checkedLanguages[language.code]}
+                    onCheckedChange={() => handleCheckboxChange(language.code)}
+                  />
+                  <FormLabel>{language.name}</FormLabel>
+                  <FormControl>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant="outline"
+                          className="justify-start ml-3"
+                          disabled={!checkedLanguages[language.code]}
+                        >
+                          {selectedUsers[language.code]?.length > 0
+                            ? `${selectedUsers[language.code]
+                                .map(
+                                  (user) => user.lastName + " " + user.firstName
+                                )
+                                .join(", ")}`
+                            : "Choose linguists"}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-[200px] p-0" align="start">
+                        <UserList
+                          users={
+                            selectedTargetLanguages.find(
+                              (lang) => lang.code === language.code
+                            )?.users || []
+                          }
+                          selectedUsers={selectedUsers[language.code] || []}
+                          onUserSelection={(users) =>
+                            handleUserSelection(language.code, users)
+                          }
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              key={index}
+              name={`isQaNeeded[${index}]` as any}
+              render={() => (
+                <FormItem>
+                  <Checkbox
+                    className="mr-3"
+                    disabled={!checkedLanguages[language.code]}
+                    checked={!!isQaNeededState[language.code]}
+                    onCheckedChange={() =>
+                      handleQaCheckboxChange(language.code)
+                    }
+                  />
+                  <FormLabel>QA needed</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      placeholder="Add QA instructions"
+                      disabled={!isQaNeededState[language.code]}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </>
         ))}
         <FormField
           control={form.control}
--- src/data/company.ts
+++ src/data/company.ts
@@ -1,23 +1,35 @@
 interface CreateCompany {
-    firstName: string;
-    lastName: string;
-    companyName: string;
-    userId: string;
+  firstName: string;
+  lastName: string;
+  companyName: string;
+  userId: string;
 }
 
 export async function createCompany(value: CreateCompany) {
-    try {
-        const response = await fetch("http://localhost:9999/company", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(value),
-        });
-        const data = await response.json();
-        return {success: data};
-    } catch (error) {
-        alert(`Error: ${error.message || "An error occurred"}`);
-        return {error: {message: error.message}};
-    }
+  try {
+    const response = await fetch("http://localhost:9999/company", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(value),
+    });
+    const data = await response.json();
+    return { success: data };
+  } catch (error) {
+    alert(`Error: ${error.message || "An error occurred"}`);
+    return { error: { message: error.message } };
+  }
 }
+
+export const fetchUserCompany = async (userId: string) => {
+  try {
+    const response = await fetch(
+      `http://localhost:9999/company/find-by-user-id?userId=${userId}`
+    );
+    const data = await response.json();
+    return data.companyId;
+  } catch (error) {
+    console.error("Error fetching user company:", error);
+  }
+};
